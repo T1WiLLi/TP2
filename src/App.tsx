4349bcd0083907d5087b3248322fe1f14ed315ab
T1WiLLi <williambeaudun@gmail.com>
2024-02-28 11:32:51 -0500

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar";
+import ParticleBackground from "./components/particles";
 import "./styles/app.css";
 
 function App() {
   return (
     <>
       <Navbar/>
+      <ParticleBackground/>
       <div className="lol"></div>
     </>
   )
