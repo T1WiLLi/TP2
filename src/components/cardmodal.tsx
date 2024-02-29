8acefd00efb74bab8e3fc15ed494bdad4758b01d
T1WiLLi <williambeaudun@gmail.com>
2024-02-29 12:23:43 -0500

    fixed css animation and modal

--- src/components/cardmodal.tsx
+++ src/components/cardmodal.tsx
@@ -1,4 +1,5 @@
-// CardModal.tsx
+import { useEffect, useState } from "react";
+import "../styles/components/cardmodal.css"
 
 interface ModalProps {
     isOpen: boolean;
@@ -16,14 +17,41 @@ const CardModal: React.FC<ModalProps> = ({ isOpen, onClose, gameName, gameData }
     if (!isOpen || !gameData.header1 || !gameData.text1 || !gameData.header2 || !gameData.text2) {
         return null;
     }
+    const [isClosing, setIsClosing] = useState(false);
+
+    useEffect(() => {
+        const handleScrollBlock = (event: Event) => {
+            if (isOpen) {
+                event.preventDefault();
+            }
+        };
+
+        document.body.style.overflow = isOpen ? 'hidden' : 'unset';
+        document.addEventListener('scroll', handleScrollBlock, {passive: false});
+
+        return () => {
+            document.body.style.overflow = 'unset';
+            document.removeEventListener('scroll', handleScrollBlock);
+        };
+    }, [isOpen]);
+
+    const handleCloseModal = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+            onClose();
+            isOpen = false;
+            setIsClosing(false);
+        }, 500);
+    };
 
     return (
-        <div className="modal fade show" tabIndex={-1} style={{ display: 'block' }} aria-modal="true">
+        <div className={`modal-wrapper ${isClosing ? `close-animation` : ''}`}>
+            <div className="modal-backdrop" onClick={onClose}></div>
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">{gameName}</h5>
-                        <button type="button" className="btn-close" onClick={onClose}></button>
+                        <button type="button" className="btn-close" onClick={handleCloseModal}></button>
                     </div>
                     <div className="modal-body">
                         <div className="modal-section">
