bbc262dc5afc6070ce81d1c25a7a96158e13308e
T1WiLLi <williambeaudun@gmail.com>
2024-02-29 10:05:24 -0500

    added a carousel for the bg

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,42 @@
 import Navbar from "../components/navbar";
 import "../styles/pages/home.css";
-import bg_image from "../assets/images/carousel/ac_mirage.webp";
+import acMirage from "../assets/images/carousel/ac_mirage.webp";
+import acValhalla from "../assets/images/carousel/ac_valhalla.jpeg";
+import acOdyssey from "../assets/images/carousel/ac_odyssey.jpg";
+import acOrigin from "../assets/images/carousel/ac_origin.jpg";
+
+interface ImageMap {
+    [gameNum: string]: string;
+}
 
 function Home() {
+    const imageMap: ImageMap = {
+        "1": acMirage,
+        "2": acValhalla,
+        "3": acOdyssey,
+        "4": acOrigin
+    };
     return (
         <>
             <Navbar/>
             <section className="landing-section">
                 <div className="background-wrapper">
-                    <img src={bg_image} alt="" />
+                    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src={imageMap[1]} className="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={imageMap[2]} className="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={imageMap[3]} className="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={imageMap[4]} className="d-block w-100" alt="..."/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="header-quote">
                     <h1>We Make <span>Awesome Games</span></h1>
