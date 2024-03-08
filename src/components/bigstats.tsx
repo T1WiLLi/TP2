8895f1125da61dc7bb9ef8a518667c258b17e674
William <williambeaudun@gmail.com>
2024-03-08 14:12:01 -0500

    almost finished stats sections

--- src/components/bigstats.tsx
+++ src/components/bigstats.tsx
@@ -1,25 +1,62 @@
 import "../styles/components/bigstats.css";
-import { useEffect } from "react";
-import AOS from "aos";
 import "aos/dist/aos.css";
+import acMirage from "../assets/images/carousel/ac_mirage.webp";
+import acValhalla from "../assets/images/carousel/ac_valhalla.jpeg";
+import acOdyssey from "../assets/images/carousel/ac_odyssey.jpg";
+import acOrigin from "../assets/images/carousel/ac_origin.jpg";
+
+interface ImageMap {
+    [gameNum: string]: string;
+}
 
 function BigStat() {
-    useEffect(() => {
-        AOS.init({ duration: 800 });
-    }, []);
+
+    const imageMap: ImageMap = {
+        "1": acMirage,
+        "2": acValhalla,
+        "3": acOdyssey,
+        "4": acOrigin
+    };
 
     return (
         <div className="wrapper-stat">
+            <div className="background-wrapper">
+                <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={imageMap[1]} className="d-block w-100" alt="..."/>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={imageMap[2]} className="d-block w-100" alt="..."/>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={imageMap[3]} className="d-block w-100" alt="..."/>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={imageMap[4]} className="d-block w-100" alt="..."/>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <h2 data-aos="fade-up">We <span>Are</span></h2>
             <div className="stats">
                 <div className="stat" data-aos="fade-up" data-aos-delay="100">
-                    <h3><span>862k+</span> monthly players</h3>
+                    <div className="stat-content">
+                        <h3>862k+</h3>
+                        <h3>Monthly players</h3>
+                    </div>
                 </div>
                 <div className="stat" data-aos="fade-up" data-aos-delay="200">
-                    <h3><span>76+</span> Million in Gross revenues annually</h3>
+                    <div className="stat-content">
+                        <h3>76+</h3>
+                        <h3>Millions in revenues</h3>
+                    </div>
                 </div>
                 <div className="stat" data-aos="fade-up" data-aos-delay="300">
-                    <h3><span>230+</span> Proud Developers</h3>
+                    <div className="stat-content">
+                        <h3>230+</h3>
+                        <h3>Proud Developers</h3>
+                    </div>
                 </div>
             </div>
         </div>
