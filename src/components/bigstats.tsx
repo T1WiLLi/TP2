18d2263ad0a758303a4fd2474fed2a1ad226d59d
T1WiLLi <williambeaudun@gmail.com>
2024-03-08 22:51:27 -0500

    fixed bigstats css and bg

--- src/components/bigstats.tsx
+++ src/components/bigstats.tsx
@@ -4,18 +4,23 @@ import acMirage from "../assets/images/carousel/ac_mirage.webp";
 import acValhalla from "../assets/images/carousel/ac_valhalla.jpeg";
 import acOdyssey from "../assets/images/carousel/ac_odyssey.jpg";
 import acOrigin from "../assets/images/carousel/ac_origin.jpg";
+import { useEffect } from "react";
+import AOS from "aos";
 
 interface ImageMap {
     [gameNum: string]: string;
 }
 
 function BigStat() {
+    useEffect(() => {
+        AOS.init();
+    }, []);
 
     const imageMap: ImageMap = {
         "1": acMirage,
         "2": acValhalla,
         "3": acOdyssey,
-        "4": acOrigin
+        "4": acOrigin,
     };
 
     return (
@@ -23,41 +28,28 @@ function BigStat() {
             <div className="background-wrapper">
                 <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src={imageMap[1]} className="d-block w-100" alt="..."/>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={imageMap[2]} className="d-block w-100" alt="..."/>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={imageMap[3]} className="d-block w-100" alt="..."/>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={imageMap[4]} className="d-block w-100" alt="..."/>
-                        </div>
+                        {Object.values(imageMap).map((image, index) => (
+                            <div className={`carousel-item ${index === 0 ? "active" : ""}`} key={index}>
+                                <img src={image} className="d-block w-100" alt="..." />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
-            <h2 data-aos="fade-up">We <span>Are</span></h2>
+            <h2 className="bg-stat__header" data-aos="zoom-in">We <span>Are</span></h2>
             <div className="stats">
-                <div className="stat" data-aos="fade-up" data-aos-delay="100">
-                    <div className="stat-content">
-                        <h3>862k+</h3>
-                        <h3>Monthly players</h3>
+                {[
+                    { count: "862k+", label: "Monthly players" },
+                    { count: "$76+", label: "Million in revenues" },
+                    { count: "230+", label: "Proud Developers" },
+                ].map((item, index) => (
+                    <div className="stat" data-aos="fade-up" data-aos-delay={`${index * 100 + 100}`} key={index}>
+                        <div className="stat-content">
+                            <h3>{item.count}</h3>
+                            <h3>{item.label}</h3>
+                        </div>
                     </div>
-                </div>
-                <div className="stat" data-aos="fade-up" data-aos-delay="200">
-                    <div className="stat-content">
-                        <h3>76+</h3>
-                        <h3>Millions in revenues</h3>
-                    </div>
-                </div>
-                <div className="stat" data-aos="fade-up" data-aos-delay="300">
-                    <div className="stat-content">
-                        <h3>230+</h3>
-                        <h3>Proud Developers</h3>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
