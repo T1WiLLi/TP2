752ac642d18ff5be87b42a1e1ac0feadce8003db
T1WiLLi <williambeaudun@gmail.com>
2024-03-11 09:31:49 -0400

    changes to the bigstats css

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,11 @@
 import Navbar from "../components/navbar";
 import "../styles/pages/home.css";
-import acMirage from "../assets/images/carousel/ac_mirage.webp";
-import acValhalla from "../assets/images/carousel/ac_valhalla.jpeg";
-import acOdyssey from "../assets/images/carousel/ac_odyssey.jpg";
-import acOrigin from "../assets/images/carousel/ac_origin.jpg";
 import GamePortfolio from "../components/gameportfolio";
 import CookieConsent from "../utils/cookieconsent";
 import BigStat from "../components/bigstats";
 import ParticleBackground from "../utils/particles";
 
-interface ImageMap {
-    [gameNum: string]: string;
-}
-
 function Home() {
-    const imageMap: ImageMap = {
-        "1": acMirage,
-        "2": acValhalla,
-        "3": acOdyssey,
-        "4": acOrigin
-    };
     return (
         <>
             <CookieConsent/>
